9d30524023cc7e51eeec790b9daee93aca496419
Naidu27 <naidukilaparthi27@gmail.com>
2026-09-09 20:46:50 +0530

    Update frontend API URL for Azure

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://myquizzapp-gvcfatbngzeufze0.southindia-01.azurewebsites.net/api",
   headers: {
     "Content-Type": "application/json",
   },
